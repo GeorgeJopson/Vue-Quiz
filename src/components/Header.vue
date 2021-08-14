c7948560deca4fe45d3c9cd298e145339f4ab8ab
George Jopson <georgejopson@gmail.com>
2021-08-14 16:28:54 +0100

    Created basic components and style

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <h1 class="title">My Quiz</h1>
+    <h3 class="counter">Counter: 4/10</h3>
+  </div>
+</template>
+<style scoped>
+.header{
+  background-color: aliceblue;
+  padding: 10px;
+  display: block;
+  margin:0;
+  text-align: left;
+}
+.title{
+  display:inline-block;
+  margin-right: 20px;
+}
+.counter{
+  display: inline-block;
+}
+</style>
